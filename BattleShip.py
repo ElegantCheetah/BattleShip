260c469aa82e05ae2f7dc67dc0b75be959959cdf
ElegantCheetah <Zain4utah@gmail.com>
2021-04-21 14:06:43 -0600

    Version 1.1.0 of BattleShip

--- BattleShip.py
+++ BattleShip.py
@@ -21,6 +21,7 @@ for x in range(5):
 
 #Function for printing the board:
 def print_board(board):
+    print('\n')
     print("Battle Field")
     print("\n")
     for row in board:
@@ -40,6 +41,7 @@ ship_col = random_col(board)
 gamedone = 1
 #for attempt in range(gamedone):
 while gamedone == 1:
+    print_board(board)
     print("\n")
     guess_row = int(input("Guess row: "))
     guess_col = int(input("Guess col: "))
@@ -51,8 +53,10 @@ while gamedone == 1:
     else:
         if(guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
             print("That's not in the board")
+            print_board(board)
         elif(board[guess_row][guess_col] == 'X'):
             print('Already hit')
+            print_board(board)
         else:
             print('miss')
             board[guess_row][guess_col] = 'X'
