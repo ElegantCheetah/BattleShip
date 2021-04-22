858c92a6569c850723716689a7786ceeef23ef44
ElegantCheetah <Zain4utah@gmail.com>
2021-04-22 09:26:30 -0600

    Version 1.2.0 of BattleShip

--- BattleShip.py
+++ BattleShip.py
@@ -14,16 +14,25 @@
 
 from random import randint
 
+def menu():
+    print('Welcome to BattleShip!')
+    print('')
+    print('1. Play BattleShip')
+    print('2. View scores')
+    print('3. Exit game')
+    print('')
+
+
+
 board = []
 
-for x in range(5):
-    board.append(["0"] * 5)
+
 
 #Function for printing the board:
 def print_board(board):
     print('\n')
     print("Battle Field")
-    print("\n")
+    print('')
     for row in board:
         print (" ".join(row))
 
@@ -33,31 +42,47 @@ def random_row(board):
 def random_col(board):
     return randint(0,len(board[0]) - 1)
 
-ship_row = random_row(board)
-ship_col = random_col(board)
 
 
+loop = 0
 
-gamedone = 1
-#for attempt in range(gamedone):
-while gamedone == 1:
-    print_board(board)
-    print("\n")
-    guess_row = int(input("Guess row: "))
-    guess_col = int(input("Guess col: "))
-    
-    if (guess_row == ship_row & guess_col == ship_col):
-        print("You win!")
+while (loop != 1):
+
+    menu()
+    menuInput = int(input('Enter selection 1-3: '))
+    if(menuInput == 3):
         break
+    if (menuInput == 2):
+        print('This section has not been coded yet')
+        #Put new things here
+    if(menuInput == 1):
+        board.clear()
+        for x in range(5):
+            board.append(["0"] * 5)
+        
+        ship_row = random_row(board)
+        ship_col = random_col(board)
 
-    else:
-        if(guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print("That's not in the board")
+        gamedone = 1
+        while gamedone == 1:
             print_board(board)
-        elif(board[guess_row][guess_col] == 'X'):
-            print('Already hit')
-            print_board(board)
-        else:
-            print('miss')
-            board[guess_row][guess_col] = 'X'
-            print_board(board)
+            print("\n")
+            guess_row = int(input("Guess row: "))
+            guess_col = int(input("Guess col: "))
+            print('\n')
+
+            if (guess_row == ship_row & guess_col == ship_col):
+                print("You win!")
+                break
+
+            else:
+                if(guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+                    print("That's not in the board")
+                    #print_board(board)
+                elif(board[guess_row][guess_col] == 'X'):
+                    print('Already hit')
+                    #print_board(board)
+                else:
+                    print('miss')
+                    board[guess_row][guess_col] = 'X'
+                    #print_board(board)
